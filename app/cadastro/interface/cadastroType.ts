4c7fdc8bcd963ae4c94565d838049b5531769a15
Fernando Medeiros <142501489+medeiros0@users.noreply.github.com>
2025-04-08 15:24:16 -0300

    eu tinha feito coisa errada

--- app/cadastro/interface/cadastroType.ts
+++ app/cadastro/interface/cadastroType.ts
@@ -3,9 +3,10 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    nome: z.string().min(3).regex(/^[A-Za-zÀ-ÿ\s]+$/, "Nome não pode conter números ou pontuação"),
+    nome: z.string().min(2).max(50),
     email: z.string().min(2).max(50),
     usuario: z.string().min(2).max(50),
     senha: z.string().min(2).max(50),
     confirmar_senha: z.string().min(2).max(50)
 })
+//
