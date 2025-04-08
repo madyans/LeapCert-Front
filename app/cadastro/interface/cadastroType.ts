c444d98c09bf9636d96be1a1e6feeec1930d4f77
Fernando Medeiros <142501489+medeiros0@users.noreply.github.com>
2025-04-08 15:16:56 -0300

    teste

--- app/cadastro/interface/cadastroType.ts
+++ app/cadastro/interface/cadastroType.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    nome: z.string().min(2).max(50),
+    nome: z.string().min(3).regex(/^[A-Za-zÀ-ÿ\s]+$/, "Nome não pode conter números ou pontuação"),
     email: z.string().min(2).max(50),
     usuario: z.string().min(2).max(50),
     senha: z.string().min(2).max(50),
