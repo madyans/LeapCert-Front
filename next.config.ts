53139b97935b281658ead2eb73baab7542b4cc64
Eduardo Frois <efroisdrumond@gmail.com>
2025-04-14 12:06:33 -0300

    docs: configuracao das variaveis de ambiente

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,32 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const version = "1.0.5";
+
+const development = {
   env: {
-    version: "1.0.5",
-    API_URL: "http://localhost:5036/api/"
+    API_URL: "http://localhost:5036/api/",
+    version: version + "[dev]"
   }
-};
+}
+
+const production = {
+  env: {
+    API_URL: "http://localhost:5036/api/",
+    version: version
+  }
+}
+
+function returnConfig() {
+  if (process.env.NODE_ENV == "development") {
+    console.log("development")
+    return development;
+
+  } else if (process.env.NODE_ENV == "production") {
+    console.log("production")
+    return production;
+
+  }
+}
+
+const nextConfig = returnConfig();
 
 export default nextConfig;
